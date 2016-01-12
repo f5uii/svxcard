0942399d4282f144b2acb4ae8848faf8eaa62ac8
root <root@raspberrypi.(none)>
2016-01-12 23:38:59 +0100

    initialization of structured deployment directories

---
+++ deploy.py
@@ -0,0 +1,8 @@
+
+dirconfig = "/etc/svxlink/"
+dirconfig-moduls = config + "svxlink.d/"  #Modules ID definition
+
+direvent-tcl= "/usr/share/svxlink/events.d/local/"  # SvxLink TCL script event handling subsystem
+dirsounds = "/usr/share/svxlink/sounds/" #Directory with langages directories
+dirmodules = "/usr/share/svxlink/modules.d/" 
+dirmodules.so = "/usr/lib/svxlink/" 
